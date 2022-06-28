b6f966e3fa002460dd79497df16a98f6c0301ba1
XovanaMartins <giovana.martins@engenharia.ufjf.br>
2022-06-28 20:00:31 -0300

    Incluido template para inserção de eventos da agenda. Incluido o gitignore para ignorar os arquivos compilados

--- core/views.py
+++ core/views.py
@@ -27,14 +27,24 @@ def submit_login(request):
 @login_required(login_url='login/')
 def lista_eventos(request):
     usuario = request.user
-    print(usuario)
     my_events = Evento.objects.filter(usuario=usuario)
-    print(my_events)
-
     all_events = Evento.objects.all()
-
-    print(all_events)
     dados = {'all_events': all_events, 'my_events': my_events}
-    print(dados)
-
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
